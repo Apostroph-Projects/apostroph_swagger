9763b996bc6ffba86438f7af373f8202978432ca
Sabani, Arben <Arben.Sabani@lionbridge.com>
2020-05-07 00:33:10 +0200

    Updates

---
+++ precache-manifest.d1b1b31d796fb7bfdf5e16870a56cf0b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35e4b4b641438232bb7da7425f87c6e2",
+    "url": "/apostroph_swagger/index.html"
+  },
+  {
+    "revision": "c0faebf1c5b50f7cda58",
+    "url": "/apostroph_swagger/static/css/2.e806ff4a.chunk.css"
+  },
+  {
+    "revision": "a3ab040794845328fd2f",
+    "url": "/apostroph_swagger/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c0faebf1c5b50f7cda58",
+    "url": "/apostroph_swagger/static/js/2.14c6018c.chunk.js"
+  },
+  {
+    "revision": "f43b7f2f49df9f5ae6b3ab2aaae15fff",
+    "url": "/apostroph_swagger/static/js/2.14c6018c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3ab040794845328fd2f",
+    "url": "/apostroph_swagger/static/js/main.66afd4eb.chunk.js"
+  },
+  {
+    "revision": "e39cd65025d9c5616b4e",
+    "url": "/apostroph_swagger/static/js/runtime-main.4f4b1fa3.js"
+  }
+]);
